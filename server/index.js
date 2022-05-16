c9465f061181599ab3895708271fbe2524f542fa
Fabrizio Onorio <onoriofabrizio@gmail.com>
2022-05-16 15:53:34 +0200

    added products available list and add to mongo db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,43 @@
 const express = require("express");
+const db = require("./mongodb");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+const port = process.env.PORT || 3030;
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.listen(port, () => console.log(`Hey I'm here listening on ${port}!`));
+
+app.get("/api/products", async (req, res) => {
+	try {
+		const results = await db.getProducts();
+		res.send(results);
+	} catch (err) {
+		console.log(err, "error from get all");
+	}
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.post("/api/products", async (req, res) => {
+	try {
+		const product = await req.body;
+		const results = await db.addProduct(product);
+		res.json(results);
+	} catch (err) {
+		console.log("error message is ", err.message);
+	}
 });
+
+app.use((req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+module.exports.app = app;
---
+++ server/mongodb.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const uri = process.env.URL;
+
+const getProducts = async () => {
+	const client = new MongoClient(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+
+	await client.connect();
+	const database = client.db("e-commerce-project");
+	const collection = database.collection("products");
+
+	const result = await collection.find().toArray();
+	await client.close();
+	return result;
+};
+
+const addProduct = async (product) => {
+	const client = new MongoClient(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+
+	await client.connect();
+	const database = client.db("e-commerce-project");
+	const collection = database.collection("products");
+
+	const result = await collection.insertOne(product);
+	await client.close();
+	return result;
+};
+
+module.exports.addProduct = addProduct;
+module.exports.getProducts = getProducts;
